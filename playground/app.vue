52e93eaf635888f1541cb4b7dde502c9a7287506
Windows-Explorer <danil.perfilef@yandex.ru>
2023-10-04 21:40:06 +0800

    отобрал автобус

--- playground/app.vue
+++ playground/app.vue
@@ -2,6 +2,7 @@
   <div>
     <TButton label="CLICK" />
     <!-- <TLoader /> -->
+    <iframe width="1350" height="483" src="https://www.youtube.com/embed/_KdRnnxflIo" title="Пророк Санбой и Хиккан / Animal ДжаZ - Три полоски AI Cover" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
   </div>
 </template>
 
